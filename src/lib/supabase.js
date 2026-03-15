c1f7e6160286ef55516382738609119f15ae9840
seoeun_lee <gangseo0505@gmail.com>
2026-03-15 19:15:37 +0900

    chang container position

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://cqdrpnsfvzohzknzxgww.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxZHJwbnNmdnpvaHprbnp4Z3d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NDc4MDIsImV4cCI6MjA4OTEyMzgwMn0.Se1pycvUu2l6uD87a8XjaE3ubUaB9P0E-Y-RZogz24c';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
